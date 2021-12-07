b85c5848b080e73fab894ea203268cee0d7fdbba
phil <uniphil@gmail.com>
2021-12-06 20:16:46 -0500

    use anyhow for errors
    is it better? who knows. some parts are at least not worse...

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 // lkajslfkj not sure how to activate this only for the *crate name*
 #![allow(non_snake_case)]
 
+use anyhow::Context;
 use git2::{Commit, Repository};
 use serde::{Deserialize, Serialize};
 use std::borrow::Cow;
@@ -31,11 +32,6 @@ use oauth2::{
 use oauth2::basic::BasicClient;
 use oauth2::reqwest::http_client;
 use reqwest::{StatusCode, header};
-
-
-#[derive(Debug)]
-struct Error {
-}
 
 
 #[derive(Debug, StructOpt)]
@@ -107,20 +103,21 @@ enum GitOrigin {
 
 
 impl GitOrigin {
-    fn parse(remote: &str) -> Result<GitOrigin, String> {
+    fn parse(remote: &str) -> Result<GitOrigin, anyhow::Error> {
         remote
             .strip_prefix("git@github.com:")
             .and_then(|r| r.strip_suffix(".git"))
             .map(|repo| GitOrigin::Github { repo: repo.to_owned() })
-            .ok_or_else(|| format!("Could not parse remote \"{:?}\" to an origin (only github ssh is recognized currently)", remote))
+            .with_context(|| format!("Could not parse remote \"{:?}\" to an origin (only github ssh is recognized currently)", remote))
     }
 }
 
 
-fn not_found(stream: &mut TcpStream) {
+fn not_found(stream: &mut TcpStream) -> Result<(), std::io::Error> {
     let response = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
-    stream.write_all(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
+    stream.write_all(response.as_bytes())?;
+    stream.flush()?;
+    Ok(())
 }
 
 
@@ -166,13 +163,13 @@ fn handle_token_followup(req: Request) -> Heard<()> {
 }
 
 
-fn listen_for<T, F>(handler: F, status: &str, response: String, listener: &TcpListener) -> Result<T, Error>
+fn listen_for<T, F>(handler: F, status: &str, response: String, listener: &TcpListener) -> Result<T, std::io::Error>
     where F: Fn(Request) -> Heard<T> {
     // terrible terrible hacky lil local http server
     for s in listener.incoming() {
-        let mut stream = s.unwrap();
+        let mut stream = s?;
         let mut buffer = [0; 1024];
-        stream.read_exact(&mut buffer).unwrap();
+        stream.read_exact(&mut buffer)?;
 
         let mut req = Request::new(&mut [EMPTY_HEADER; 0]);
 
@@ -181,7 +178,7 @@ fn listen_for<T, F>(handler: F, status: &str, response: String, listener: &TcpLi
             HttpResult::Err(TooManyHeaders) => {}, // we allocated zero headers, so this is expected
             HttpResult::Err(e) => {
                 eprintln!("Ignoring request that could not be parsed: {:?}", e);
-                not_found(&mut stream);
+                not_found(&mut stream)?;
                 continue
             },
         };
@@ -189,20 +186,20 @@ fn listen_for<T, F>(handler: F, status: &str, response: String, listener: &TcpLi
         match handler(req) {
             Heard::Ya(stuff) => {
                 let resp = format!("HTTP/1.1 {}\r\nContent-Length: {}\r\n\r\n{}", status, response.len(), response);
-                stream.write_all(resp.as_bytes()).unwrap();
-                stream.flush().unwrap();
+                stream.write_all(resp.as_bytes())?;
+                stream.flush()?;
                 return Ok(stuff)
             },
             Heard::Ohno(msg) => {
                 eprintln!("{}", msg);
-                not_found(&mut stream);
+                not_found(&mut stream)?;
             },
         }
     }
     unreachable!()
 }
 
-fn oauth() -> StandardTokenResponse<EmptyExtraTokenFields, BasicTokenType> {
+fn oauth() -> Result<StandardTokenResponse<EmptyExtraTokenFields, BasicTokenType>, anyhow::Error> {
     // bind early so we can bail if the port is not available
     let listener = TcpListener::bind(&format!("{}:{}", LOCAL_AUTH_HOST, LOCAL_AUTH_PORT))
         .unwrap_or_else(|e| panic!("Could not bind to port {} for oauth redirect listener: {:?}", LOCAL_AUTH_PORT, e));
@@ -214,7 +211,7 @@ fn oauth() -> StandardTokenResponse<EmptyExtraTokenFields, BasicTokenType> {
             AuthUrl::new(format!("{}/oauth/auth", COMMIT_BLOG_HOST)).expect("auth url"),
             Some(TokenUrl::new(format!("{}/oauth/token", COMMIT_BLOG_HOST)).expect("token url"))
         )
-        .set_redirect_uri(RedirectUrl::new(format!("http://{}:{}/oauth/authorized", LOCAL_AUTH_HOST, LOCAL_AUTH_PORT)).expect("redirect url"));
+        .set_redirect_uri(RedirectUrl::new(format!("http://{}:{}/oauth/authorized", LOCAL_AUTH_HOST, LOCAL_AUTH_PORT))?);
 
     let (pkce_challenge, pkce_verifier) = PkceCodeChallenge::new_random_sha256();
 
@@ -231,38 +228,29 @@ fn oauth() -> StandardTokenResponse<EmptyExtraTokenFields, BasicTokenType> {
     }
 
     println!("waiting for auth redirect...");
-    let (code, state) = listen_for(handle_auth_redirect, "200 OK", include_str!("authorized.html").to_string(), &listener).unwrap();
+    let (code, state) = listen_for(handle_auth_redirect, "200 OK", include_str!("authorized.html").to_string(), &listener)?;
 
     assert_eq!(&state, csrf_token.secret(), "csrf check");
 
-    let token_result =
+    let token =
         client
             .exchange_code(AuthorizationCode::new(code))
             .set_pkce_verifier(pkce_verifier)
             .add_extra_param("client_id", "commit--cli")  // ??? seems like this wasn't sending??
-            .request(http_client);
+            .request(http_client)?;
 
-    match token_result {
-        Ok(tok) => {
-            listen_for(handle_token_followup, "200 OK", "got token".to_string(), &listener).unwrap();
-            tok
-        },
-        Err(e) => {
-            eprintln!("could not get token: {:?}", e);
-            panic!("token")
-        },
-    }
+    listen_for(handle_token_followup, "200 OK", "got token".to_string(), &listener)?;
+    Ok(token)
 }
 
 
-fn post(commit: Commit, origin: GitOrigin, token: StoredToken) {
+fn post(commit: Commit, origin: GitOrigin, token: StoredToken) -> Result<(), reqwest::Error> {
     let client = reqwest::blocking::Client::new();
     let resp = client.put(&format!("{}/api/blog/{}", COMMIT_BLOG_HOST, commit.id()))
         .header(header::USER_AGENT, "commit--cli hacky test version")
         .bearer_auth(token.to_bearer())
         .json(&origin)
-        .send()
-        .expect("to send ok");
+        .send()?;
 
     match resp.status() {
         code if code.is_success() =>  {
@@ -276,20 +264,20 @@ fn post(commit: Commit, origin: GitOrigin, token: StoredToken) {
             panic!("Got unexpected non-success response status: {:?}", otherwise)
         }
     }
+    Ok(())
 }
 
-fn unpost(commit: Commit, origin: GitOrigin, token: StoredToken) {
+fn unpost(commit: Commit, origin: GitOrigin, token: StoredToken) -> Result<(), reqwest::Error> {
     let client = reqwest::blocking::Client::new();
     let resp = client.delete(&format!("{}/api/blog/{}", COMMIT_BLOG_HOST, commit.id()))
         .header(header::USER_AGENT, "commit--cli hacky test version")
         .bearer_auth(token.to_bearer())
         .json(&origin)
-        .send()
-        .expect("to send ok");
+        .send()?;
 
     match resp.status() {
         code if code.is_success() =>  {
-            println!("ok, deleted");
+            println!("ok, unposted.");
         },
         StatusCode::BAD_REQUEST => {
             eprintln!("Failed to unpost: {:?}", resp.text());
@@ -298,6 +286,7 @@ fn unpost(commit: Commit, origin: GitOrigin, token: StoredToken) {
             panic!("Got unexpected non-success response status: {:?}", otherwise)
         }
     }
+    Ok(())
 }
 
 fn get_token(entry: Entry) -> StoredToken {
@@ -310,10 +299,9 @@ fn get_token(entry: Entry) -> StoredToken {
 }
 
 
-fn get_commit(repo: &Repository, git_ref: Option<String>) -> Result<Commit, String> {
+fn get_commit(repo: &Repository, git_ref: Option<String>) -> Result<Commit, git2::Error> {
     repo.revparse_single(&git_ref.unwrap_or_else(|| "HEAD".to_string()))
         .and_then(|obj| obj.peel_to_commit())
-        .map_err(|e| e.message().to_owned())
 }
 
 
@@ -331,7 +319,7 @@ fn get_likely_origin(repo: &Repository) -> Option<GitOrigin> {
 }
 
 
-fn main() {
+fn main() -> Result<(), anyhow::Error> {
     let entry = Entry::new("commit--blog", "auth");
     match Blog::from_args() {
         Blog::Login => {
@@ -344,34 +332,31 @@ fn main() {
                     }
                 }
             } else {
-                let token = oauth();
+                let token = oauth()?;
                 let storable = StoredToken::from_token_response(token);
                 let j = serde_json::to_string(&storable).expect("jsonify");
-                match entry.set_password(&j) {
-                    Ok(()) => println!("ya, new set"),
-                    Err(err) => eprintln!("naaa setting: {:?}", err),
-                }
+                entry.set_password(&j)?;
+                println!("new access token saved.")
             }
         },
         Blog::Logout => {
-            match entry.delete_password() {
-                Ok(()) => println!("ok password deleted"),
-                Err(err) => eprintln!("error deleting pw: {:?}", err),
-            }
+            entry.delete_password()?;
+            println!("access token deleted.")
         },
         Blog::Post { git_ref } => {
             let token = get_token(entry);
-            let repo = Repository::discover(env::current_dir().unwrap()).expect("open current repo");
-            let commit = get_commit(&repo, git_ref).expect("to find commit");
+            let repo = Repository::discover(env::current_dir()?)?;
+            let commit = get_commit(&repo, git_ref)?;
             let origin = get_likely_origin(&repo).expect("origin to be found");
-            post(commit, origin, token)
+            post(commit, origin, token)?
         },
         Blog::Unpost { git_ref } => {
             let token = get_token(entry);
-            let repo = Repository::discover(env::current_dir().unwrap()).expect("open current repo");
-            let commit = get_commit(&repo, git_ref).expect("to find commit");
+            let repo = Repository::discover(env::current_dir()?)?;
+            let commit = get_commit(&repo, git_ref)?;
             let origin = get_likely_origin(&repo).expect("origin to be found");
-            unpost(commit, origin, token)
+            unpost(commit, origin, token)?
         },
     }
+    Ok(())
 }
